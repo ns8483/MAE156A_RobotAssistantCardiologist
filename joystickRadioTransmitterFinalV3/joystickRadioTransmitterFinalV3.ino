d8533b99a5b230cc29680c3c4433476cd899d491
ns8483 <130114877+ns8483@users.noreply.github.com>
2024-05-14 18:24:35 -0700

    Add files via upload

--- joystickRadioTransmitterFinalV3/joystickRadioTransmitterFinalV3.ino
+++ joystickRadioTransmitterFinalV3/joystickRadioTransmitterFinalV3.ino
@@ -33,16 +33,32 @@ int UD_MAX = 100;
 
 void setup() {
   Serial.begin(115200);
+
+  //set up digital pins for joystick switch
+  pinMode(RIGHT_SWITCH, OUTPUT);
+  pinMode(LEFT_SWITCH, OUTPUT);
+  digitalWrite(RIGHT_SWITCH, HIGH);
+  digitalWrite(LEFT_SWITCH, HIGH);
+
   radio.begin();
   radio.openWritingPipe(addresses[1]);
-  radio.openReadingPipe(0, addresses[0]);
+  radio.openReadingPipe(1, addresses[0]);
   radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
 
   //setting up LCD display
   lcd.init();
   lcd.clear();         
   lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("T24 Cardio Robot");
+  lcd.setCursor(0,1);
+  lcd.print("Connecting...");
+  delay(700);
+  lcd.setCursor(0,2);
+  lcd.print("Connected!");
+  delay(500);
+  lcd.clear();
+  
   lcd.setCursor(0, 0);
   lcd.print("R/L Flex: ");
   lcd.setCursor(0, 1);
@@ -54,21 +70,104 @@ void setup() {
 }
 
 void loop() {
+  radio.stopListening();
   delay(20);
   //Sends it to radio for communication
-  int joyPos[4];
-  int joyPosArray[4];
-  joyPos[0]= analogRead(RIGHT_JOY_X);
+  /*int joyPos[4];
+  int joyPosArray[4];*/
+  int joyPosArray[5];
+  /*joyPos[0]= analogRead(RIGHT_JOY_X);
   joyPos[1]= analogRead(RIGHT_JOY_Y);
   joyPos[2] = analogRead(LEFT_JOY_X);
-  joyPos[3] = analogRead(LEFT_JOY_Y);
-  joyPosArray[0]= map(joyPos[0], 0, 1023, -100, 100);
-  joyPosArray[1]= map(joyPos[1], 0, 1023, 100, -100);
-  joyPosArray[2]= map(joyPos[2], 0, 1023, 100,-100);
-  joyPosArray[3]= map(joyPos[3], 0, 1023, -100,100);
-  radio.write(&joyPosArray, sizeof(joyPosArray));
+  joyPos[3] = analogRead(LEFT_JOY_Y);*/
+  joyPosArray[0]= map(analogRead(RIGHT_JOY_X), 0, 1023, -100, 100);
+  joyPosArray[1]= map(analogRead(RIGHT_JOY_Y), 0, 1023, 100, -100);
+  joyPosArray[2]= map(analogRead(LEFT_JOY_X), 0, 1023, 100,-100);
+  joyPosArray[3]= map(analogRead(LEFT_JOY_Y), 0, 1023, -100,100);
+  //for joyPosArray[4], 0 means no change, -1 means tip flextion step decreases, 1 means tip flexion step increases, 
+    //-2 means rotation step decreases, 2 means rotation step increases, 
+      //-3 means lin actuator step decreases, 3 means lin actuator step increases
+  joyPosArray[4] = 0;
+  if (digitalRead(RIGHT_SWITCH) == 0) {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Exit Menu");
+    lcd.setCursor(0,1);
+    lcd.print("+ or - F Step");
+    lcd.setCursor(0,2);
+    lcd.print("+ or - R Step");
+    lcd.setCursor(0,3);
+    lcd.print("10x or 0.1x T Step ");
+    
+    delay(100);
+    int downCount = 0;
+    while(digitalRead(RIGHT_SWITCH) == 1) {
+      if (map(analogRead(RIGHT_JOY_Y), 0, 1023, 100, -100) == -100) {
+        lcd.setCursor(19, downCount);
+        lcd.print(" ");
+        if (downCount != 3) {
+          downCount = downCount + 1;
+        } else {
+          downCount = 0;
+        }
+      }
+      else if (map(analogRead(RIGHT_JOY_Y), 0, 1023, 100, -100) == 100){
+        lcd.setCursor(19, downCount);
+        lcd.print(" ");
+        if (downCount != 0) {
+        downCount = downCount - 1;
+        } else {
+          downCount = 3;
+        }
+      }
+      Serial.println(downCount);
+      lcd.setCursor(19, downCount%4);
+      lcd.print("*");
+      delay(400);
+    }
+    if (downCount == 0){
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("EXITING...");
+    }
+    if (downCount > 0){
+      lcd.clear();
+      lcd.setCursor(1,0);
+      lcd.print("<- (-)");
+      lcd.setCursor(13,0);
+      lcd.print("(+) ->");
+      delay(300);
+      while(true){
+        if (digitalRead(RIGHT_SWITCH) == 0) {
+          lcd.clear();
+          lcd.print("STEP INCREASED");
+          break;
+        }
+        if (digitalRead(LEFT_SWITCH)==0) {
+          lcd.clear();
+          lcd.print("STEP DECREASED");
+          downCount = downCount * -1;
+          break;
+        }
+      }
+    }
+    joyPosArray[4] = downCount;
+    Serial.println(downCount);
+    delay(500);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("R/L Flex: ");
+    lcd.setCursor(0, 1);
+    lcd.print("U/D Flex: ");
+    lcd.setCursor(0, 2);
+    lcd.print("Rotation: ");
+    lcd.setCursor(0, 3);
+    lcd.print("Translation: ");
+  }
+
+  radio.write(&joyPosArray, sizeof(joyPosArray));  
   radio.startListening();
-  delay(100);
+  delay(20);
   if (radio.available()) {
     int posMetrics[4];
     radio.read(&posMetrics, sizeof(posMetrics));
@@ -76,18 +175,35 @@ void loop() {
     botServo = String(posMetrics[1]);
     stepper = String(posMetrics[2]);
     linActuator = String(posMetrics[3]);
+    for (int i = 0; i < 3; i++){
+      if (posMetrics[i]<10){
+        lcd.setCursor(11, i);
+        lcd.print(" ");
+      }
+      else if(posMetrics[i]<100){
+        lcd.setCursor(12, i);
+        lcd.print(" ");
+      }
+    }
+    if (posMetrics[3]<10){
+      lcd.setCursor(14, 3);
+      lcd.print(" ");
+    }
+    else if (posMetrics[3]<100){
+      lcd.setCursor(15,3);
+      lcd.print(" ");
+    }
     lcd.setCursor(10, 0);
-    lcd.print(topServo);
+    lcd.print(topServo + char(223));
     Serial.println("top servo: " + topServo);
     lcd.setCursor(10, 1);
-    lcd.print(botServo);
+    lcd.print(botServo + char(223));
     Serial.println("bot servo: " + botServo);
     lcd.setCursor(10, 2);
-    lcd.print(stepper);
+    lcd.print(stepper + char(223));
     Serial.println("stepper: " + stepper);
     lcd.setCursor(13, 3);
-    lcd.print(linActuator);
+    lcd.print(linActuator + " mm");
     Serial.println("lin actuator: " + linActuator);
-    radio.stopListening();
   }
 }
